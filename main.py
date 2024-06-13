5a9df6d2c3cf8407e4a7f71928c743de5303a671
baochau-lampart <baochau.lampart@gmail.com>
2024-06-13 11:27:52 +0700

    push readme.md

--- main.py
+++ main.py
@@ -22,7 +22,8 @@
 # async def read_item(skip: int = 0, limit: int = 10):
 #     return fake_items_db[skip : skip + limit] # trả về dữ liệu từ skip đến skip + limit
 
-from fastapi import FastAPI, HTTPException, Depends, status #import class FastAPI() từ thư viện fastapi
+# import class FastAPI() từ thư viện fastapi
+from fastapi import FastAPI, HTTPException, Depends, status
 from pydantic import BaseModel
 from typing import Annotated
 import models
@@ -32,13 +33,16 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
+
 class PostBase(BaseModel):
     title: str
     content: str
     user_id: int
 
+
 class UserBase(BaseModel):
     username: str
+
 
 def get_db():
     db = SessionLocal()
@@ -47,7 +51,9 @@ def get_db():
     finally:
         db.close()
 
+
 db_dependency = Annotated[Session, Depends(get_db)]
+
 
 @app.post("/users/", status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserBase, db: db_dependency):
@@ -55,9 +61,42 @@ async def create_user(user: UserBase, db: db_dependency):
     db.add(db_user)
     db.commit()
 
+
 @app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
 async def read_user(user_id: int, db: db_dependency):
     user = db.query(models.User).filter(models.User.id == user_id).first()
     if user is None:
         raise HTTPException(status_code=404, detail='User not found')
     return user
+
+
+@app.delete("/users/{user_id}", status_code=status.HTTP_202_ACCEPTED)
+async def delete_user(user_id: int, db: db_dependency):
+    user = db.query(models.User).filter(models.User.id == user_id).first()
+    if user is None:
+        raise HTTPException(status_code=404, detail='User not found')
+    db.delete(user)
+    db.commit()
+
+
+@app.post("/posts/", status_code=status.HTTP_201_CREATED)
+async def create_post(post: PostBase, db: db_dependency):
+    db_post = models.Post(**post.dict())
+    db.add(db_post)
+    db.commit()
+
+
+@app.delete("/posts/{post_id}", status_code=status.HTTP_200_OK)
+async def delete_post(post_id: int, db: db_dependency):
+    db_post = db.query(models.Post).filter(models.Post.id == post_id).first()
+    if db_post is None:
+        raise HTTPException(status_code=404, detail="Post was not found")
+    db.delete(db_post)
+    db.commit()
+
+@app.put("/posts/{post_id}", status_code=status.HTTP_200_OK)
+async def update_post(post_id: int, db: db_dependency):
+    db_post = db.query(models.Post).filter(models.Post.id== post_id).first()
+    if db_post is None:
+        raise HTTPException(status_code=404, detail="Post was not found")
+    
