a7cc6d3fb87f118c973ae2b74ae7dd046e4e5017
baochau-lampart <baochau.lampart@gmail.com>
2024-06-14 12:47:58 +0700

    Update JWT

--- main.py
+++ main.py
@@ -1,31 +1,7 @@
-# from fastapi import FastAPI, HTTPException, Depends, status #import class FastAPI() từ thư viện fastapi
-
-# app = FastAPI() # gọi constructor và gán vào biến app
-
-
-# @app.get("/") # giống flask, khai báo phương thức get và url
-# async def root(): # do dùng ASGI nên ở đây thêm async, nếu bên thứ 3 không hỗ trợ thì bỏ async đi
-#     return {"message": "Hello World"}
-
-# # @app.get("/items/{item_id}")
-# # async def read_item(item_id: int):
-# #     return {"item_id": item_id}
-
-# @app.get("/files/{file_path:path}")
-# async def read_file(file_path: str):
-#     return {"file_path": file_path}
-
-# fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}] # pair format: key-value
-
-
-# @app.get("/items/")
-# async def read_item(skip: int = 0, limit: int = 10):
-#     return fake_items_db[skip : skip + limit] # trả về dữ liệu từ skip đến skip + limit
-
-# import class FastAPI() từ thư viện fastapi
 from fastapi import FastAPI, HTTPException, Depends, status
+from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from pydantic import BaseModel
-from typing import Annotated
+from typing import Annotated, Optional
 import models
 from database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -33,16 +9,7 @@ from sqlalchemy.orm import Session
 app = FastAPI()
 models.Base.metadata.create_all(bind=engine)
 
-
-class PostBase(BaseModel):
-    title: str
-    content: str
-    user_id: int
-
-
-class UserBase(BaseModel):
-    username: str
-
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 def get_db():
     db = SessionLocal()
@@ -51,16 +18,64 @@ def get_db():
     finally:
         db.close()
 
-
 db_dependency = Annotated[Session, Depends(get_db)]
 
+class PostBase(BaseModel):
+    title: str
+    content: str
+    user_id: int
 
+class UserBase(BaseModel):
+    username: str
+    email: Optional[str] = None
+    full_name: Optional[str] = None
+
+def get_user(db, username: str):
+    if username in db:
+        user_dict = db[username]
+        return user_dict
+    
+def fake_decode_token(token):
+    user = get_user(fake_users_db, token)
+    return user
+
+async def get_current_user(token: str = Depends(oauth2_scheme)):
+    user = fake_decode_token(token)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid authentication credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    return user
+
+fake_users_db = {
+    "johndoe": {
+        "username": "johndoe",
+        "full_name": "John Doe",
+        "email": "johndoe@example.com",
+        "password": "secret",
+    },
+}
+
+# API for authenticate
+@app.post("/login")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    user_dict = fake_users_db.get(form_data.username)
+    if not username == user["username"]:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+    password = form_data.password
+    if not password == user["password"]:
+        raise HTTPException(status_code=400, detail="Incorrect username or password")
+
+    return {"access_token": user["username"], "token_type": "bearer"}
+
+# API for user
 @app.post("/users/", status_code=status.HTTP_201_CREATED)
 async def create_user(user: UserBase, db: db_dependency):
     db_user = models.User(**user.dict())
     db.add(db_user)
     db.commit()
-
 
 @app.get("/users/{user_id}", status_code=status.HTTP_200_OK)
 async def read_user(user_id: int, db: db_dependency):
@@ -68,7 +83,6 @@ async def read_user(user_id: int, db: db_dependency):
     if user is None:
         raise HTTPException(status_code=404, detail='User not found')
     return user
-
 
 @app.delete("/users/{user_id}", status_code=status.HTTP_202_ACCEPTED)
 async def delete_user(user_id: int, db: db_dependency):
@@ -78,7 +92,7 @@ async def delete_user(user_id: int, db: db_dependency):
     db.delete(user)
     db.commit()
 
-
+# API for post
 @app.post("/posts/", status_code=status.HTTP_201_CREATED)
 async def create_post(post: PostBase, db: db_dependency):
     db_post = models.Post(**post.dict())
